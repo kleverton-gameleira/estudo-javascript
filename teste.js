09e3751ba4fb5e4365162b1c6fa6bebcaae732f6
kleverton gameleira <vetongameleira@gmail.com>
2021-04-02 14:52:28 -0300

    iniciando o rastreio do meu estudo em javascript com o curso da geek university

--- teste.js
+++ teste.js
@@ -1,41 +1,19 @@
-function soma(a,b){
-    let soma = a + b
-    alert(`A soma entre os valores ${a} e ${b} é igual a ${soma}`)
-}
-function mult(a,b){
-    let mult = a*b
-    alert(`A multiplicacao dos valores ${a} e ${b} é ${mult}`)
-}
-function sub(a,b){
-    if (a >= b){
-        let sub = a - b
-        alert(`A subtração de ${a} e ${b} é igual a ${sub}`)
-    }else{
-        let sub = b - a
-        alert(`A subtração de ${a} e ${b} é igual a ${sub}`)
+function testeExterno(){
+    let num = document.getElementById('num').value
+    /*for(var i =0 ; i <=10; i++){
+        //alert(`${num} x ${i} = ${num*i}`)
+        document.write(`${num} x ${i} = ${num*i}</br>`)
     }
-}
-function div(a,b){
-    let div = a / b
-    alert(`A divisao de ${a} e ${b} é igual a ${div}`)
-}
-function calcular(op){
-    op = document.getElementById('op').value
-    switch(op){
-        case 's':
-            soma(Number(document.getElementById('n1').value),Number(document.getElementById('n2').value))
-            break
-        case 'mult':
-            mult(Number(document.getElementById('n1').value),Number(document.getElementById('n2').value))
-            break;
-        case 'sub':
-            sub(Number(document.getElementById('n1').value),Number(document.getElementById('n2').value))
-            break;
-        case 'div':
-            div(Number(document.getElementById('n1').value),Number(document.getElementById('n2').value))
-            break;
-        default:
-            alert("Voce deve informar valores validos para que a operação seja realizada da forma correta!!!!")
-    }
+    let i = 0
+    while(i <= 10){
+        document.write(`${num} x ${i} = ${num*i}</br>`)
+        i++
+    }*/
 
+    let cont = 0 
+    do{
+        document.write(`${num} x ${cont} = ${num*cont}</br>`)
+        cont++
+    }while(cont <= 10)
+    
 }
