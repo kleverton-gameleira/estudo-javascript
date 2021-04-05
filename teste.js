a3e4d83cf08691cdf98845fab827648dbe858cb3
kleverton gameleira <vetongameleira@gmail.com>
2021-04-05 13:59:06 -0300

    alteracoes feitos nos docs de glossario e teste

--- teste.js
+++ teste.js
@@ -1,19 +1,3 @@
-function testeExterno(){
-    let num = document.getElementById('num').value
-    /*for(var i =0 ; i <=10; i++){
-        //alert(`${num} x ${i} = ${num*i}`)
-        document.write(`${num} x ${i} = ${num*i}</br>`)
-    }
-    let i = 0
-    while(i <= 10){
-        document.write(`${num} x ${i} = ${num*i}</br>`)
-        i++
-    }*/
-
-    let cont = 0 
-    do{
-        document.write(`${num} x ${cont} = ${num*cont}</br>`)
-        cont++
-    }while(cont <= 10)
+function teste(){
     
 }
