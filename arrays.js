bf7d348ae80d507f3fc04165754c4457e580d9cd
KLEVERTON de SOUSA GAMELEIRA <x52118843291@tcu.gov.br>
2021-04-13 17:51:08 -0300

    estudos sobre map e foreach completados

--- arrays.js
+++ arrays.js
@@ -1,5 +1,13 @@
-let nums = [1,2,3,4,5,6]
-nums.push(7,8,9,10)
-nums.forEach(function(num,ind){
-    console.log(`${num} x 2 = ${num*2}`)
-})
+let nums = [1,2,3,4,5,6,7,8,9,10]
+
+function soma(num){
+    return num += num
+}
+
+function div(num){
+    return num / 2
+}
+
+let res = nums.map(div)
+console.log(`Array Original: ${nums}`)
+console.log(`Array Alterado: ${res}`)
